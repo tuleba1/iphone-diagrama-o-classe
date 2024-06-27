a435435defe5907823a48f61e1c6e78ffb880a1c
Túlio Dantas <tuliopragana@hotmail.com>
2024-06-26 21:24:05 -0300

    Atualizações no reprodutor musical

--- src/MeuReprodutorMusical.java
+++ src/MeuReprodutorMusical.java
@@ -1,2 +1,23 @@
-public class MeuReprodutorMusical {
+public class MeuReprodutorMusical implements ReprodutorMusical {
+    private String musicaAtual;
+
+    public void tocar(){
+        System.out.println("Tocando música: " + musicaAtual);
+    }
+
+    public void pausar(){
+        System.out.println("Pausar música " + musicaAtual);
+    }
+    public void selecionarMusica(String musica){
+        this.musicaAtual = musica;
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    public static void main(String[] args) {
+        MeuReprodutorMusical reprodutor = new MeuReprodutorMusical();
+        reprodutor.selecionarMusica("JAY-Z - Empire State Of Mind ft. Alicia Keys");
+        reprodutor.tocar();
+        reprodutor.pausar();
+    }
+
 }
