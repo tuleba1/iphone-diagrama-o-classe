9b359dbfe5d948cd5bff41a0f4b401887b0d8ca8
Túlio Dantas <tuliopragana@hotmail.com>
2024-06-26 22:23:02 -0300

    Mudança de detalhes na classe principal

--- src/Iphone.java
+++ src/Iphone.java
@@ -7,13 +7,16 @@ public class Iphone {
         reprodutor.selecionarMusica("JAY-Z - Empire State Of Mind ft. Alicia Keys");
         reprodutor.tocar();
         reprodutor.pausar();
+        System.out.println("");
 
         navegador.exibirPagina("htpps://www.exemplo.com.br");
         navegador.adicionarNovaAba();
         navegador.atualizarPagina();
+        System.out.println("");
 
         telefone.ligar("+55 81 1234-5678");
         telefone.atender();
         telefone.iniciarCorreioVoz();
+        System.out.println("");
     }
 }
